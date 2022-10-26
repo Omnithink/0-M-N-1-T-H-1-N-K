7fa4c15bc29b62e6b97dd91a829e8fd15fadba0f
Christian <christian0draws@gmail.com>
2022-10-26 15:13:06 -0400

    fixed index transition

--- js/app.js
+++ js/app.js
@@ -87,3 +87,26 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+window.onload = () => {
+    const anchors = document.querySelectorAll('a');
+    const transition_el = document.querySelector('.transition');
+  
+    setTimeout(() => {
+      transition_el.classList.remove('is-active');
+    }, 500);
+  
+    for (let i = 0; i < anchors.length; i++) {
+      const anchor = anchors[i];
+  
+      anchor.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target.href;
+  
+       transition_el.classList.add('is-active');
+
+        setInterval(() => {
+          window.location.href = target;
+        }, 500);
+      })
+    }
+  }
