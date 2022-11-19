580250655e0bb21f2749e4a6f25f92e62f939a65
Christian <christian0draws@gmail.com>
2022-11-19 10:56:00 -0500

    Fixed transition on contact page

---
+++ js/dist/transition.dev.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.onload = function () {
+  var anchors = document.querySelectorAll('header, .banner-button');
+  var transition_el = document.querySelector('.transition');
+  setTimeout(function () {
+    transition_el.classList.remove('is-active');
+  }, 500);
+
+  for (var i = 0; i < anchors.length; i++) {
+    var anchor = anchors[i];
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      var target = e.target.href;
+      transition_el.classList.add('is-active');
+      setInterval(function () {
+        window.location.href = target;
+      }, 1000);
+    });
+  }
+};
